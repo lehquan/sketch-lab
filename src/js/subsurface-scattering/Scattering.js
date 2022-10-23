0740a2a5ab77ea151bf679960df319c0e2ab1539
QuanLe <quan.le1926@hotmail.com>
2022-10-23 21:21:12 +0900

    fix: spotlight.

--- src/js/subsurface-scattering/Scattering.js
+++ src/js/subsurface-scattering/Scattering.js
@@ -1,25 +1,25 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader';
+import * as THREE from 'three'
+import { TWEEN } from 'three/addons/libs/tween.module.min';
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
-import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js';
-import {SubsurfaceScatteringShader} from 'three/examples/jsm/shaders/SubsurfaceScatteringShader';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass';
-import { FXAAShader } from 'three/addons/shaders/FXAAShader';
-import { BleachBypassShader } from 'three/addons/shaders/BleachBypassShader';
-import { GammaCorrectionShader } from 'three/addons/shaders/GammaCorrectionShader';
-import { ColorCorrectionShader} from 'three/addons/shaders/ColorCorrectionShader';
+import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js'
+import {SubsurfaceScatteringShader} from 'three/examples/jsm/shaders/SubsurfaceScatteringShader'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass'
+import { FXAAShader } from 'three/addons/shaders/FXAAShader'
+import { BleachBypassShader } from 'three/addons/shaders/BleachBypassShader'
+import { GammaCorrectionShader } from 'three/addons/shaders/GammaCorrectionShader'
+import { ColorCorrectionShader} from 'three/addons/shaders/ColorCorrectionShader'
 
 class Scattering {
   constructor() {
-    this.scene = new THREE.Scene();
     this.sssShader = SubsurfaceScatteringShader
     this.gltfLoader = new GLTFLoader()
     this.textureLoader = new THREE.TextureLoader()
-    
+    this.radius = 200
     this.effectFXAA = null
     this.composer = null
     this.model = null
@@ -40,7 +40,7 @@ class Scattering {
         0.1,
         10000
     );
-    this.camera.position.set(0, 0, 6);
+    this.camera.position.set(0, 0, 8);
     
     // renderer
     this.renderer = new THREE.WebGLRenderer();
@@ -48,52 +48,96 @@ class Scattering {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.outputEncoding = THREE.sRGBEncoding;
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping
+    // this.renderer.logarithmicDepthBuffer = true
     this.renderer.shadowMap.enabled = true
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
-    this.renderer.setClearColor( 0x0d1113 );
+    this.renderer.shadowMap.type = THREE.VSMShadowMap
+    this.renderer.autoClear = false
     container.appendChild(this.renderer.domElement);
     
     // scene
+    this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xE5E5E5)
     new RGBELoader().load('assets/future_parking_1k.hdr', texture => {
       texture.mapping = THREE.EquirectangularReflectionMapping
       this.scene.environment = texture
     })
-    this.light()
     
-    // COMPOSER
-    this.createComposer()
-    
-    // controls
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
-    controls.update()
-    
-    this.#render()
-    window.addEventListener("resize", this.#onWindowResize);
-  };
-  
-  light = () => {
+    // light
     this.scene.add( new THREE.AmbientLight( 0x9e9e9e, 1 ) );
-    // this.scene.add( new THREE.DirectionalLight( 0x9e9e9e, 1 ) );
-
     const front = new THREE.PointLight( 0x9e9e9e, 1, 300 )
     front.position.set(0, 1, 5)
     this.scene.add( front );
-
-    const bottom_2 = new THREE.PointLight( 0x9e9e9e, 1, 2 )
-    bottom_2.power = 100
-    bottom_2.position.set(0, -4, 3)
-    this.scene.add( bottom_2 );
-  }
+    
+    this.spotLightMesh = new THREE.Mesh( new THREE.SphereGeometry( 1/10, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x888888, transparent: true, opacity: 0 } ) );
+    this.spotLight = new THREE.SpotLight()
+    this.spotLight.castShadow = true
+    this.spotLight.shadow.mapSize.width = 512
+    this.spotLight.shadow.mapSize.height = 512
+    this.spotLight.shadow.camera.near = 0.5
+    this.spotLight.shadow.camera.far = 100
+    this.spotLightMesh.add(this.spotLight);
+    this.spotLightMesh.position.set(0, 3, 0)
+    this.scene.add( this.spotLightMesh );
+  
+    /*this.spotLight1 = this.createSpotlight( 0xFF7F00 );
+    this.spotLight2 = this.createSpotlight( 0x00FF7F );
+    this.spotLight3 = this.createSpotlight( 0x7F00FF );
+    this.spotLight1.position.set( 5, 25, 35 );
+    this.spotLight2.position.set( 0, 25, 35 );
+    this.spotLight3.position.set( -5, 25, 35 );
+    this.scene.add( this.spotLight1, this.spotLight2, this.spotLight3 );*/
+    
+    // COMPOSER
+    this.initComposer()
+    
+    // controls
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.maxDistance = 10
+    controls.minDistance = 5
+    controls.update()
+    
+    this.#render()
+    // this.animateSpotLight()
+    window.addEventListener("resize", this.#onWindowResize);
+  };
   
   build = async () => {
-    // load model
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath('lib/vendor/draco/gltf/')
     dracoLoader.preload()
     this.gltfLoader.setDRACOLoader(dracoLoader)
     await this.loadModel()
     await this.makeUniform()
+  }
+  
+  createSpotlight = ( color ) => {
+    
+    const newObj = new THREE.SpotLight( color, 2 );
+    
+    newObj.castShadow = true;
+    newObj.angle = 0.3;
+    newObj.penumbra = 0.2;
+    newObj.decay = 2;
+    newObj.distance = 50;
+    
+    return newObj;
+    
+  }
+  
+  tween = ( light ) => {
+    new TWEEN.Tween( light ).to( {
+      angle: ( Math.random() * 0.7 ) + 0.1,
+      penumbra: Math.random() + 1
+    }, Math.random() * 3000 + 2000 )
+    .easing( TWEEN.Easing.Quadratic.Out ).start();
+    
+    new TWEEN.Tween( light.position ).to( {
+      x: ( Math.random() * 30 ) - 15,
+      y: ( Math.random() * 10 ) + 15,
+      z: ( Math.random() * 30 ) - 15
+    }, Math.random() * 3000 + 2000 )
+    .easing( TWEEN.Easing.Quadratic.Out ).start();
+    
   }
   
   /**
@@ -110,6 +154,11 @@ class Scattering {
         this.model.position.set(0, -5.5, 0)
         this.scene.add(this.model)
         
+        this.model.traverse(child => {
+          child.receiveShadow = true
+          child.castShadow = true
+        })
+        
         // find object needs to apply SSS
         this.head = this.model.getObjectByName('Head_Head_0')
         this.eye = this.model.getObjectByName('Eye_Eye_0')
@@ -124,23 +173,23 @@ class Scattering {
       // const thicknessTexture = this.textureLoader.load( 'https://threejs.org/examples/models/fbx/bunny_thickness.jpg' );
       
       // head
-      const headMap = this.textureLoader.load('assets/texture/angelica/diffuse.jpg')
+      const headMap = this.textureLoader.load('assets/texture/diffuse.jpg')
       headMap.encoding = THREE.sRGBEncoding
       headMap.flipY = false
-      const headSpecularMap = this.textureLoader.load('assets/texture/angelica/specular.png')
+      const headSpecularMap = this.textureLoader.load('assets/texture/specular.png')
       headSpecularMap.encoding = THREE.sRGBEncoding
-      const headNormalMap = this.textureLoader.load('assets/texture/angelica/normal.jpg')
+      const headNormalMap = this.textureLoader.load('assets/texture/normal.jpg')
       headNormalMap.encoding = THREE.sRGBEncoding
-      const headAOMap = this.textureLoader.load('assets/texture/angelica/occlusion.png')
+      const headAOMap = this.textureLoader.load('assets/texture/occlusion.png')
       headAOMap.encoding = THREE.sRGBEncoding
-      const thicknessTexture = this.textureLoader.load( 'assets/texture/angelica/diffuse.jpg' );
+      const thicknessTexture = this.textureLoader.load( 'assets/texture/diffuse.jpg' );
       thicknessTexture.flipY = false
       thicknessTexture.encoding = THREE.sRGBEncoding
       
       // eye
-      const eyeMap = this.textureLoader.load('assets/texture/angelica/eye_diffuse.jpg')
+      const eyeMap = this.textureLoader.load('assets/texture/eye_diffuse.jpg')
       eyeMap.encoding = THREE.sRGBEncoding
-      const eyeSpecularMap = this.textureLoader.load('assets/texture/angelica/eye_diffuse.jpg')
+      const eyeSpecularMap = this.textureLoader.load('assets/texture/eye_diffuse.jpg')
       eyeSpecularMap.encoding = THREE.sRGBEncoding
       
       // make uniform
@@ -150,15 +199,15 @@ class Scattering {
         faceUniforms.specularMap.value = headSpecularMap
         faceUniforms.normalMap.value = headNormalMap
         faceUniforms.aoMap.value = headAOMap
-        faceUniforms.normalScale.value = new THREE.Vector2(0.8, 0.8);
+        // faceUniforms.normalScale.value = new THREE.Vector2(0.8, 0.8);
   
         // faceUniforms[ 'thicknessMap' ].value = thicknessTexture;
-        faceUniforms[ 'thicknessColor' ].value = new THREE.Vector3( 0.5, 0.3, 0.0 );
-        faceUniforms[ 'thicknessDistortion' ].value = 0.1;
-        faceUniforms[ 'thicknessAmbient' ].value = 0.4;
-        faceUniforms[ 'thicknessAttenuation' ].value = 0.8;
-        faceUniforms[ 'thicknessPower' ].value = 2.0;
-        faceUniforms[ 'thicknessScale' ].value = 16.0;
+        // faceUniforms.thicknessColor.value = new THREE.Vector3( 0.5, 0.3, 0.0 );
+        // faceUniforms.thicknessDistortion.value = 0.1;
+        // faceUniforms.thicknessAmbient.value = 0.4;
+        // faceUniforms.thicknessAttenuation.value = 0.8;
+        // faceUniforms.thicknessPower.value = 2.0;
+        // faceUniforms.thicknessScale.value = 1.0;
         
         this.convertMat(this.head, faceUniforms, headMap)
       }
@@ -166,8 +215,13 @@ class Scattering {
         const eyeUniforms = THREE.UniformsUtils.clone(this.sssShader.uniforms)
         eyeUniforms.map.value = eyeMap
         eyeUniforms.specularMap.value = eyeSpecularMap
-        eyeUniforms.normalScale.value = new THREE.Vector2(0.8, 0.8);
-        eyeUniforms.uniforms.thicknessScale.value = 1;
+        // eyeUniforms.normalScale.value = new THREE.Vector2(0.8, 0.8);
+        // eyeUniforms.thicknessScale.value = 1;
+        // eyeUniforms.thicknessColor.value = new THREE.Vector3( 0.5, 0.3, 0.0 );
+        // eyeUniforms.thicknessDistortion.value = 0.1;
+        // eyeUniforms.thicknessAmbient.value = 0.4;
+        // eyeUniforms.thicknessAttenuation.value = 0.8;
+        // eyeUniforms.thicknessPower.value = 2.0;
         this.convertMat(this.eye, eyeUniforms, eyeMap)
       }
     })
@@ -187,20 +241,19 @@ class Scattering {
     object.material.needsUpdate = true
   }
   
-  createComposer = () => {
-    // this.renderPass = new RenderPass( this.scene, this.camera)
-    // this.effectFXAA = new ShaderPass( FXAAShader )
-    // this.effectFXAA.uniforms["resolution"].value.set(1/window.innerWidth, 1/window.innerHeight)
-    // this.gammaPass = new ShaderPass( GammaCorrectionShader )
-    //
-    // this.composer = new EffectComposer( this.renderer )
-    // this.composer.setSize( window.innerWidth, window.innerHeight );
-    // this.composer.setPixelRatio( window.devicePixelRatio );
-    // this.composer.addPass( this.renderPass );
-    // this.composer.addPass( this.effectFXAA );
-    // this.composer.addPass( this.gammaPass );
+  initComposer = () => {
+    /*this.renderPass = new RenderPass( this.scene, this.camera)
+    this.effectFXAA = new ShaderPass( FXAAShader )
+    this.effectFXAA.uniforms["resolution"].value.set(1/window.innerWidth, 1/window.innerHeight)
+    this.gammaPass = new ShaderPass( GammaCorrectionShader )
+
+    this.composer = new EffectComposer( this.renderer )
+    this.composer.setSize( window.innerWidth, window.innerHeight );
+    this.composer.setPixelRatio( window.devicePixelRatio );
+    this.composer.addPass( this.renderPass );
+    this.composer.addPass( this.effectFXAA );
+    this.composer.addPass( this.gammaPass );*/
     
-    this.renderer.autoClear = false;
     const renderModel = new RenderPass(this.scene, this.camera)
     const effectBleach = new ShaderPass(BleachBypassShader)
     const effectColor = new ShaderPass(ColorCorrectionShader)
@@ -217,7 +270,7 @@ class Scattering {
     
     this.composer = new EffectComposer( this.renderer )
     this.composer.addPass(renderModel)
-    this.composer.addPass(this.effectFXAA)
+    // this.composer.addPass(this.effectFXAA)
     this.composer.addPass(effectBleach)
     this.composer.addPass(effectColor)
     this.composer.addPass(gammaPass)
@@ -235,11 +288,24 @@ class Scattering {
     this.#render();
   };
   
+  animateSpotLight = () => {
+    this.tween( this.spotLight1 );
+    this.tween( this.spotLight2 );
+    this.tween( this.spotLight3 );
+    setTimeout( this.animateSpotLight, 5000 );
+  }
+  
   #render = () => {
-
+    let t = performance.now() / 2000
+    // TWEEN.update();
+    
+    if (this.spotLightMesh) {
+      this.spotLightMesh.position.set(Math.cos(t), 0, -Math.sin(t)).multiplyScalar(this.radius);
+    }
+    
     requestAnimationFrame(this.#render)
     // this.renderer.render(this.scene, this.camera); // don't use this if using composer
-    this.composer.render()
+    this.composer.render(0.01)
   };
 }
 
