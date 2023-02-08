3bd39c76c4bac57c6fa9c9479155baf08f201405
Le Hong Quan <quan.le@letsee.io>
2023-02-08 14:12:30 +0900

    feat: add demo.

---
+++ 18-dispose/src/Experience/Environment.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three'
+import Experience from './Experience'
+
+export default class Environment {
+  constructor() {
+    this.experience = new Experience()
+    this.scene = this.experience.scene
+    this.resources = this.experience.resources
+
+    // Wait for resources
+    this.resources.on("ready", () => {
+      this.setEnv()
+    });
+  }
+  setEnv = () => {
+    this.scene.background = new THREE.Color(0xa5c9a5)
+    this.scene.add( new THREE.DirectionalLight(0xffffff, 2))
+    this.scene.add( new THREE.AmbientLight(0xffffff, 2))
+  }
+  update = () => {
+    this.experience.update()
+  }
+}
---
+++ 18-dispose/src/Experience/sources.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "testMatcap",
+    type: "texture",
+    path: "assets/textures/matcap.png",
+    source: "https://opengameart.org/content/fox-and-shiba"
+  },
+  {
+    name: "foxModel",
+    type: "gltfModel",
+    path: "assets/models/Fox/glTF/Fox.gltf",
+    source: "https://opengameart.org/content/fox-and-shiba"
+  },
+  {
+    name: "tontu",
+    type: "gltfModel",
+    path: "assets/models/popup_card_A.glb",
+    source: "https://youtu.be/D2DRxffm7Yk"
+  },
+  {
+    name: "cherry",
+    type: "gltfModel",
+    path: "assets/models/cherry_blossoms.glb",
+    source: "https://skfb.ly/6XqvQ"
+  },
+];
