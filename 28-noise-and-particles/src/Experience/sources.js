7de8ce9e2292c74336ec544f1c20937e5388c313
Le Hong Quan <quan.le@letsee.io>
2023-04-06 16:48:04 +0900

    wip: add new demo

---
+++ 28-noise-and-particles/src/Experience/sources.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "icon",
+    type: "svg",
+    path: "assets/textures/icon.svg",
+    source: ""
+  },
+  {
+    name: "royal_esplanade",
+    type: "hdrTexture",
+    path: "assets/textures/royal_esplanade_1k.hdr",
+    source: "https://polyhaven.com/a/future_parking"
+  },
+]
